37b73256218baab720c2a7fb2ead476a685777b6
Micky Mari <micky.mari@etu.u-bordeaux.fr>
2025-11-17 18:04:24 +0100

    fix tour.py: - amélioration gestion des tours

--- tour.py
+++ tour.py
@@ -1,7 +1,7 @@
+""" Module gérant le tour des équipes dans le jeu."""
 def reset_actions_tour(jeu):
+    """ Réinitialise les actions des unités au début du tour. """
     for u in jeu.unites:
         if u.equipe == jeu.tour and u.vivant:
             u.reset_actions()
-            
-            if hasattr(u, 'debut_tour'):
-                u.debut_tour(jeu.unites, jeu, jeu.q_range, jeu.r_range)
+            u.debut_tour(jeu.unites, jeu, jeu.q_range, jeu.r_range)
